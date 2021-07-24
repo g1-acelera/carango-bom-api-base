ad765dc53e9273939d12dbfc68719ae79590776e
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-23 22:38:14 -0300

    Adicionei o ExistentMarcaService à herança em MarcaRepository

--- src/main/java/br/com/caelum/carangobom/repository/MarcaRepository.java
+++ src/main/java/br/com/caelum/carangobom/repository/MarcaRepository.java
@@ -1,10 +1,12 @@
 package br.com.caelum.carangobom.repository;
 
 import br.com.caelum.carangobom.model.entity.Marca;
+import br.com.caelum.carangobom.service.ExistentMarcaService;
+
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface MarcaRepository extends CrudRepository<Marca, Long> {
+public interface MarcaRepository extends CrudRepository<Marca, Long>, ExistentMarcaService {
     List<Marca> findAllByOrderByNome();
 }
