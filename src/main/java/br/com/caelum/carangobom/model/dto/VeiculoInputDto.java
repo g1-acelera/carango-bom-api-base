24c2eb10ad479b4ec859160df10b4d1ff11ccebb
Gustavo Jardim <gustavo.spadotto@gmail.com>
2021-07-23 22:37:15 -0300

    Atualizei o atributo Marca marca para long marcaId na VeiculoInputDto

--- src/main/java/br/com/caelum/carangobom/model/dto/VeiculoInputDto.java
+++ src/main/java/br/com/caelum/carangobom/model/dto/VeiculoInputDto.java
@@ -4,18 +4,22 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import br.com.caelum.carangobom.model.entity.Marca;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VeiculoInputDto {
 
 	@NotBlank
 	private String nome;
 	
 	@NotNull
-	private Marca marca;
+	private long marcaId;
 	
 	@NotNull
 	private int ano;
